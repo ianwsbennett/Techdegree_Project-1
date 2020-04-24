39a448a4e9e73dfbea5f78dc99add47cf96b4c25
Ian Bennett <ianwsbennett@gmail.com>
2020-04-24 00:11:54 -0400

    Added various functions

--- js/script.js
+++ js/script.js
@@ -11,29 +11,66 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-let quotes = [
-  {quote: 'test', author: 'test'},
-  {quote: 'test', author: 'test'},
-  {quote: 'test', author: 'test'},
-  {quote: 'test', author: 'test', year: 1986},
-  {quote: 'test', author: 'test', source: 'test'},
+
+// Random quote array with various properties  
+let randomQuote = [
+  {quote: 'Be king, in your dreams. Immense power is acquired by assuring yourself in your secret reveries that you were born to control affairs',
+   source: 'Andrew Carnegie'},
+
+  {quote: 'A thing is funny when—in some way that is not actually offensive or frightening—it upsets the established order.', 
+    source: 'George Orwell'},
+
+  {quote: 'Either write something worth reading or do something worth writing.', 
+    source: 'Benjamin Franklin'},
+
+  {quote: 'I’m as proud of many of the things we haven’t done as the things we have done. Innovation is saying no to a thousand things.', 
+    source: 'Steve Jobs'},
+
+  {quote: 'Success has ruined many a man.', 
+    source: 'Benjamin Franklin', 
+    citation: "Poor Richard's Almanack", 
+    year: 1752},
 ];
+
 
 /***
  * `getRandomQuote` function
 ***/
 
 
+// Global Variable for getting random number 
+let randomNumber = Math.floor(Math.random() * randomQuote.length);
 
-/***
- * `printQuote` function
-***/
 
+// Function to get random quote from array  
+function getRandomQuote() {
+  let finalQuote = randomQuote[randomNumber];
+  return finalQuote;
+}
+
+
+function printQuote() {
+  finalQuote = getRandomQuote();
+  let text = ``;
+  text += `<p class="quote"> ${finalQuote.quote} </p>`; 
+  text += `<p class="source"> ${finalQuote.source}`;
+
+  if (finalQuote.citation){
+    text += `<span class="citation"> ${finalQuote.citation}</span>`;
+  }
+  if (finalQuote.year){
+    text += `<span class="year"> ${finalQuote.year}</span>`;
+  }
+  
+  text += `</p>`;
+  document.getElementById('quote-box').innerHTML = text; 
+}
 
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
+
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
